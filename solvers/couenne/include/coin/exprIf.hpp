58e4ca7c83c1f92e42bec46095b9972763eb567f
glhrmprd <guilherme.paula@klabin.com.br>
2025-09-30 19:48:47 -0300

    adding solvers folder

---
+++ solvers/couenne/include/coin/exprIf.hpp
@@ -0,0 +1,20 @@
+/* $Id: exprIf.hpp 217 2009-07-08 17:02:07Z pbelotti $
+ *
+ * Name:    exprIf.hpp
+ * Author:  Pietro Belotti
+ * Purpose: definition of the exprIf class that implements the ifnl AMPL operator
+ *
+ * (C) Carnegie-Mellon University, 2007. 
+ * This file is licensed under the Common Public License (CPL)
+ */
+
+#ifndef EXPRIF_HPP
+#define EXPRIF_HPP
+
+#include "exprOp.hpp"
+
+class exprIf: public exprOp {
+
+}
+
+#endif
